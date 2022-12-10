9eb9d084e4297dcb1bf09d1ddb0e057492827b55
Sajijola1 <sajijola@gmail.com>
2022-12-10 16:10:06 +0000

    Reverted changes to pass task1

--- milestone_3.py
+++ milestone_3.py
@@ -1,41 +1,14 @@
 import random
 
-
 word_list = ['bananas', 'apples', 'oranges', 'mangoes', 'blackberries']
-
-
-print(word_list) 
 
 word = random.choice(word_list)
 
-
-print(word) 
-
-def check_guess(char):
+while True:
+    guess = input('Guess a single letter: ')
     
-    
-    char = char.lower()
-    
-    
-    if char in word:
-        print(f"Good guess! {char} is in the word.")
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+        break
     else:
-        print(f"Sorry, {char} is not in the word. Try again.")
-
-def ask_for_input():
-    while True:
-        
-        guess = input('Guess a single letter: ')
-        
-        
-        if len(guess) == 1 and guess.isalpha():
-            print("Good guess!")
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-    
-    
-    check_guess(guess)
-
-
-ask_for_input()
+        print("Invalid letter. Please, enter a single alphabetical character.")
