389206c98cca04633fa09cbb389b3088029054de
Sajijola1 <sajijola@gmail.com>
2022-12-11 01:27:15 +0000

    modifies file using functions

--- milestone_3.py
+++ milestone_3.py
@@ -4,16 +4,25 @@ word_list = ['bananas', 'apples', 'oranges', 'mangoes', 'blackberries']
 
 word = random.choice(word_list)
 
-while True:
-    guess = input('Guess a single letter: ')
-    
-    if len(guess) == 1 and guess.isalpha():
-        print("Good guess!")
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+def check_guess(guess):
+    guess = guess.lower()
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def ask_for_input():
+    while True:
+        guess = input('Guess a single letter: ')
+        
+        if len(guess) == 1 and guess.isalpha():
+            print("Good guess!")
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    check_guess(guess)
+
+ask_for_input()
