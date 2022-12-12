f1e652915c186384506f760416527c5222be11db
Sajijola1 <sajijola@gmail.com>
2022-12-12 02:06:54 +0000

    Modifies Milestone4 file for OOP concepts

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class Hangman():
     def __init__(self, word_list, num_lives=5) -> None:
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
-        self.num_letters = set(self.word) # Using the set() function to convert the word to a set of unique characters
+        self.num_letters = set(self.word) 
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
